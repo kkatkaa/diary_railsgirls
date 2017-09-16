a1624cfc62ddf5ac2b930040e78290f3eab0b3f6
kkatkaa <katka36@tlen.pl>
2017-09-16 17:33:39 +0200

    Updates show.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index', to: 'welcome#index'
-  get '/', to: 'welcome#index'
-  get 'dziennik', to: 'welcome#index'
+  get '/', to: 'entries#index'
   get 'entries/', to: 'entries#index'
-  get 'entries/:id', to: 'entries#show', as: "entry" 
+  get 'entries/:id', to: 'entries#show', as: "entry"
 end
